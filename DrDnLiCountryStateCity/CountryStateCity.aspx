f3a9acf5fae67c74bb41e3fb2e37e96b7117143e
Srikanth <srikanth.chilamkuri@zensar.com>
2021-02-08 08:37:42 +0530

    Country State City Select DropDown LIst added

---
+++ DrDnLiCountryStateCity/CountryStateCity.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CountryStateCity.aspx.cs" Inherits="DrDnLiCountryStateCity.CountryStateCity" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Choose Country"></asp:Label>
+        <asp:DropDownList ID="DropDownList1" runat="server" AppendDataBoundItems="True" AutoPostBack="True" Height="46px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem>--Choose Country--</asp:ListItem>
+        </asp:DropDownList>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Choose State"></asp:Label>
+            <asp:DropDownList ID="DropDownList2" runat="server" AppendDataBoundItems="True" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+                <asp:ListItem>--Choose State--</asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <asp:Label ID="Label3" runat="server" Text="Choose City"></asp:Label>
+        <asp:DropDownList ID="DropDownList3" runat="server" AppendDataBoundItems="True" AutoPostBack="True">
+            <asp:ListItem>--Choose City--</asp:ListItem>
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ DrDnLiCountryStateCity/CountryStateCity.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DrDnLiCountryStateCity
+{
+    public partial class CountryStateCity : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable dt = null;
+            if (!Page.IsPostBack)
+            {
+                using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString()))
+                {
+                    cn.Open();
+                    using (SqlCommand cmd = new SqlCommand("select CountryName from Country", cn))
+                    {
+                        using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                        {
+
+
+                           
+                                dt = new DataTable();
+                                dt.Load(dr);
+
+                                DropDownList1.DataSource = dt;
+
+                            
+                                DropDownList1.DataTextField = "CountryName";
+                                
+
+                                DropDownList1.DataBind();
+                            
+
+
+                        }
+
+
+                    }
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = null;
+
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString()))
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("StateSe", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("CountryName", DropDownList1.SelectedValue);
+                    using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+
+                        DropDownList2.Items.Clear();
+                        DropDownList2.Items.Add("--Please choose State--");
+                        dt = new DataTable();
+                            dt.Load(dr);
+                            DropDownList2.DataSource = dt;
+                        
+                        DropDownList2.DataTextField = "StateName";
+                            DropDownList2.DataBind();
+
+                    }
+
+                }
+
+            }
+
+
+
+
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = null;
+
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString()))
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("CitySe", cn))
+                {
+                    DropDownList3.Items.Clear();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("StateName", DropDownList2.SelectedValue);
+                    using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+
+                        
+                        dt = new DataTable();
+                            dt.Load(dr);
+                            DropDownList3.DataSource = dt;
+                        
+                        DropDownList3.DataTextField = "CityName";
+                            DropDownList3.DataBind();
+
+
+                    }
+
+                }
+            }
+
+        }
+
+    }
+}
